0471f5953cfe8d2d23c31cb39e765833c846962f
Kris <50147772+kpatel-1@users.noreply.github.com>
2021-08-31 19:26:37 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# spring-reddit-clone
+ 
---
+++ src/main/java/com/example/springredditclone/controller/AuthController.java
@@ -0,0 +1,45 @@
+/*Verification, Token Management, Return*/
+
+package com.example.springredditclone.controller;
+
+import com.example.springredditclone.dto.AuthenticationResponse;
+import com.example.springredditclone.dto.LoginRequest;
+import com.example.springredditclone.dto.RegisterRequest;
+import com.example.springredditclone.service.AuthService;
+import lombok.AllArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
+        authService.signup(registerRequest);
+        return new ResponseEntity<>("User Registration Successful",
+                HttpStatus.OK);
+    }
+
+    @GetMapping("accountVerification/{token}")
+    public ResponseEntity<String> verifyAccount(@PathVariable String token) {
+        authService.verifyAccount(token);
+        return new ResponseEntity<>("Account Activated Successfully. Thank you.", HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public AuthenticationResponse login(@RequestBody LoginRequest LoginRequest) { 
+        return authService.login(LoginRequest);
+    }
+}
