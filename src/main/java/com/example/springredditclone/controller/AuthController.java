fe8a1405d86b565a14a7c98828194c98db38611b
Kris <50147772+kpatel-1@users.noreply.github.com>
2021-08-31 19:37:16 -0400

    Logout & Refresh Tokens
    - Added logout function
    - Added refresh tokens

--- src/main/java/com/example/springredditclone/controller/AuthController.java
+++ src/main/java/com/example/springredditclone/controller/AuthController.java
@@ -1,22 +1,18 @@
-/*Verification, Token Management, Return*/
-
 package com.example.springredditclone.controller;
 
 import com.example.springredditclone.dto.AuthenticationResponse;
 import com.example.springredditclone.dto.LoginRequest;
+import com.example.springredditclone.dto.RefreshTokenRequest;
 import com.example.springredditclone.dto.RegisterRequest;
 import com.example.springredditclone.service.AuthService;
+import com.example.springredditclone.service.RefreshTokenService;
 import lombok.AllArgsConstructor;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -24,22 +20,35 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+    private final RefreshTokenService refreshTokenService;
 
     @PostMapping("/signup")
     public ResponseEntity<String> signup(@RequestBody RegisterRequest registerRequest) {
         authService.signup(registerRequest);
         return new ResponseEntity<>("User Registration Successful",
-                HttpStatus.OK);
+                OK);
     }
 
     @GetMapping("accountVerification/{token}")
     public ResponseEntity<String> verifyAccount(@PathVariable String token) {
         authService.verifyAccount(token);
-        return new ResponseEntity<>("Account Activated Successfully. Thank you.", HttpStatus.OK);
+        return new ResponseEntity<>("Account Activated Successfully", OK);
     }
 
     @PostMapping("/login")
-    public AuthenticationResponse login(@RequestBody LoginRequest LoginRequest) { 
-        return authService.login(LoginRequest);
+    public AuthenticationResponse login(@RequestBody LoginRequest loginRequest) {
+        return authService.login(loginRequest);
     }
+
+    @PostMapping("refresh/token")
+    public AuthenticationResponse refreshTokens(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
+        return authService.refreshToken(refreshTokenRequest);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(@Valid @RequestBody RefreshTokenRequest refreshTokenRequest) {
+        refreshTokenService.deleteRefreshToken(refreshTokenRequest.getRefreshToken());
+        return ResponseEntity.status(OK).body("Refresh Token Deleted Successfully!!");
+    }
+
 }
--- src/main/java/com/example/springredditclone/security/JwtProvider.java
+++ src/main/java/com/example/springredditclone/security/JwtProvider.java
@@ -1,6 +1,9 @@
 package com.example.springredditclone.security;
 
 import com.example.springredditclone.exception.SpringRedditException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
@@ -10,15 +13,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.security.*;
 import java.security.cert.CertificateException;
+import java.sql.Date;
+import java.time.Instant;
 
 import static io.jsonwebtoken.Jwts.parser;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.Claims;
+import static java.util.Date.from;
 
 @Service
 public class JwtProvider {
 
     private KeyStore keyStore;
+    @Value("${jwt.expiration.time}")
+    private Long jwtExpirationInMillis;
 
     @PostConstruct
     public void init() {
@@ -36,7 +42,18 @@ public class JwtProvider {
         org.springframework.security.core.userdetails.User principal = (User) authentication.getPrincipal();
         return Jwts.builder()
                 .setSubject(principal.getUsername())
+                .setIssuedAt(from(Instant.now()))
                 .signWith(getPrivateKey())
+                .setExpiration(Date.from(Instant.now().plusMillis(jwtExpirationInMillis)))
+                .compact();
+    }
+
+    public String generateTokenWithUserName(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(from(Instant.now()))
+                .signWith(getPrivateKey())
+                .setExpiration(Date.from(Instant.now().plusMillis(jwtExpirationInMillis)))
                 .compact();
     }
 
@@ -44,30 +61,34 @@ public class JwtProvider {
         try {
             return (PrivateKey) keyStore.getKey("springblog", "secret".toCharArray());
         } catch (KeyStoreException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
-            throw new SpringRedditException("Exception occurred while retrieving public key from keystore");
+            throw new SpringRedditException("Exception occured while retrieving public key from keystore");
         }
     }
 
     public boolean validateToken(String jwt) {
-        parser().setSigningKey(getPublicKey()).parseClaimsJws(jwt);
+        parser().setSigningKey(getPublickey()).parseClaimsJws(jwt);
         return true;
     }
 
-    private PublicKey getPublicKey() {
+    private PublicKey getPublickey() {
         try {
             return keyStore.getCertificate("springblog").getPublicKey();
-        }
-        catch (KeyStoreException e) {
-            throw new SpringRedditException("Exception occurred while retrieving public key");
+        } catch (KeyStoreException e) {
+            throw new SpringRedditException("Exception occured while " +
+                    "retrieving public key from keystore");
         }
     }
 
     public String getUsernameFromJwt(String token) {
         Claims claims = parser()
-            .setSigningKey(getPublicKey())
-            .parseClaimsJws(token)
-            .getBody();
+                .setSigningKey(getPublickey())
+                .parseClaimsJws(token)
+                .getBody();
 
-            return claims.getSubject();
+        return claims.getSubject();
+    }
+
+    public Long getJwtExpirationInMillis() {
+        return jwtExpirationInMillis;
     }
 }
